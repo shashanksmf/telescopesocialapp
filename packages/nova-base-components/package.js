1c6af28f38d7d99c5047485596017ea9c0f9ac9f
shashank <shashanksmf@outlook.com>
2017-05-14 21:24:37 +0530

    notification issue solved

--- packages/nova-base-components/package.js
+++ packages/nova-base-components/package.js
@@ -21,6 +21,7 @@ Package.onUse(function (api) {
     'tmeasday:check-npm-versions@0.3.1',
     'std:accounts-ui@1.2.6',
     'utilities:react-list-container@0.1.10',
+    'nova:notification'
   ]);
 
   api.mainModule("lib/server.js", "server");
