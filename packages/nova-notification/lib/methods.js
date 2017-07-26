dd9456d1326f66caa4b0daf856e6242c964c35be
shashank <shashanksmf@outlook.com>
2017-07-26 19:22:56 +0530

    Update methods.js

--- packages/nova-notification/lib/methods.js
+++ packages/nova-notification/lib/methods.js
@@ -1,45 +1,45 @@
-import { Meteor } from 'meteor/meteor';
+import {Meteor} from 'meteor/meteor';
 import Posts from "meteor/nova:posts";
 import Users from 'meteor/nova:users';
 import Notification from "./collection.js";
 
 Meteor.methods({
-  'notification.insert':function(notification){
-  	 Notification.insert(notification,function(error,result){
-  	 	if(error) {
-  	 		return {"response":false,"error":error}
-  	 	}
-  	 	return result;
-  	 	})
-  },
-  'notification.markRead':function(id){
+    'notification.insert': function (notification) {
+        Notification
+            .insert(notification, function (error, result) {
+                if (error) {
+                    return {"response": false, "error": error}
+                }
+                return result;
+            })
+    },
+    'notification.markRead': function (id) {
 
-    Notification.update({_id:id},{$set:{read:true}},function(error,result){
-      if(error) {
-       console.log("error server",error)
-        return error;
-      }
-      console.log("error server",result)
-       
-      return result;
-    });
-   // return true;
-  },
-  'notification.fetchunread':function(id){
-
-    Notification.find({"to":id} , function(error,result){
-      if(error) {
-        return error;
-      }
-      return result
-    });
-  }
+        Notification
+            .update({
+                _id: id
+            }, {
+                $set: {
+                    read: true
+                }
+            }, function (error, result) {
+                if (error) {
+                    return error;
+                }
+                return result;
+            });
+        // return true;
+    },
+    'notification.fetchunread': function (id) {
+        return Notification
+            .find({"to": id,read:false})
+            .fetch();
+    }
 });
 
-// assign smart methods on startup so the method code generated takes care of countries' custom fields (extended schema) -> prevent bug on create/edit countries with custom fields
+// assign smart methods on startup so the method code generated takes care of
+// countries' custom fields (extended schema) -> prevent bug on create/edit
+// countries with custom fields
 Meteor.startup(() => {
-  Notification.smartMethods({
-    createName: "notification.new",
-    editName: "notification.edit"
-  });
+    Notification.smartMethods({createName: "notification.new", editName: "notification.edit"});
 });
