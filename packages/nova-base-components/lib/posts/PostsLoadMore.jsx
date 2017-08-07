a04845c18faf78d72462ed23703aa7018d8d41a1
shashank <shashanksmf@outlook.com>
2017-08-07 12:14:10 +0530

    Load More implemented

--- packages/nova-base-components/lib/posts/PostsLoadMore.jsx
+++ packages/nova-base-components/lib/posts/PostsLoadMore.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 const PostsLoadMore = ({loadMore, count, totalCount}) => {
+	console.log("loadMore",loadMore)
+	 var that = this;
+	if(Meteor.isClient) {
+      $(".main").scroll(function() {
+        console.log("main");
+      if($(".posts-load-more") && $(".posts-load-more").length > 0 && ($(window).height() > $(".posts-load-more").offset().top)) {
+      	console.log($(window).height(),$(".posts-load-more").offset().top)
+   //   	console.log(that.re\fs);
+       		//that.refs.loadMoreRef.click();
+       		document.getElementsByClassName("posts-load-more")[0].click()
+      }
+    });
+      
+    }
+
   return (
-    <a className="posts-load-more" onClick={loadMore}>
+    <a ref="loadMoreRef" className="posts-load-more" onClick={loadMore}>
       <span><FormattedMessage id="posts.load_more"/></span>
       &nbsp;
       {totalCount ? <span className="load-more-count">{`(${count}/${totalCount})`}</span> : null}
